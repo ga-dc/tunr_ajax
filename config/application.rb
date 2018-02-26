f3984507ddbca1662118e91805a43706c663c141
Perry Fustero <pjfust@gmail.com>
2018-02-26 18:58:52 -0500

    update

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Tunr
   class Application < Rails::Application
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
-      end
-    end
 
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST PUT PATCH DELETE OPTIONS}.join(",")
+      'Access-Control-Request-Method' => %w{GET POST PUT PATCH DELETE OPTIONS}.join(","),
+      'Access-Control-Request-Method' => '*'
     }
 
     # Settings in config/environments/* take precedence over those specified here.
